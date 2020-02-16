94803c16a1d18bdfc3df79c460b9709786c4a257
Arihant Jain <arihant72600@gmail.com>
2020-02-15 18:56:31 -0800

    added basic navigation

--- App.js
+++ App.js
@@ -1,40 +1,160 @@
-import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import "react-native-gesture-handler";
+import React from "react";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ScrollView
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
+const Stack = createStackNavigator();
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Wome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Make Payments" component={MakePaymentsScreen} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Home" }}
+        />
+        <Stack.Screen name="Requests" component={RequestsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+
+function HomeScreen(x) {
+  console.log(x);
+  navigation = x.navigation;
+  return (
+    <View>
+      <View style={header_styles.container}>
+        <Text style={{ fontSize: 40 }}> Hello, Eric. </Text>
+      </View>
+
+      <ScrollView style={styles.body}>
+        <View style={styles.container}>
+          <Text style={styles.instructions}>
+            To share a photo from your phone with a friend, just press the
+            button below!
+          </Text>
+        </View>
+      </ScrollView>
+
+      <View style={footer_styles.container}>
+        <TouchableOpacity
+          style={footer_styles.button}
+          onPress={() => navigation.navigate("Make Payments", { a: "home" })}
+        >
+          <Text style={styles.buttonText}>Home</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={footer_styles.button}
+          onPress={() => navigation.navigate("Make Payments", { a: "new" })}
+        >
+          <Text style={styles.buttonText}>New</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={footer_styles.button}>
+          <Text style={styles.buttonText}>Profile</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+function MakePaymentsScreen({ route, navigation }) {
+  console.log(route.params);
+  return (
+    <View>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>{route.params.a}</Text>
+      </View>
+      <View style={footer_styles.container}>
+        <TouchableOpacity
+          style={footer_styles.button}
+          onPress={() => navigation.navigate("Requests")}
+        >
+          <Text style={styles.buttonText}>Home</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={footer_styles.button}>
+          <Text style={styles.buttonText}>Pick a photo</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={footer_styles.button}>
+          <Text style={styles.buttonText}>Profile</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+function RequestsScreen({ navigation }) {
+  return (
+    <View>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Requests Screen</Text>
+      </View>
+      <View style={footer_styles.container}>
+        <TouchableOpacity
+          style={footer_styles.button}
+          onPress={() => navigation.navigate("Home")}
+        >
+          <Text style={styles.buttonText}>Home</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={footer_styles.button}>
+          <Text style={styles.buttonText}>Pick a photo</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={footer_styles.button}>
+          <Text style={styles.buttonText}>Profile</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const header_styles = StyleSheet.create({
+  container: {
+    height: "20%"
+  }
+});
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
+    backgroundColor: "#fff",
     alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    justifyContent: "center"
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
+  body: {
+    height: "70%"
   },
   instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
+    color: "#888",
+    fontSize: 80,
+    marginHorizontal: 15,
+    marginBottom: 10
+  },
+  buttonText: {
+    fontSize: 20,
+    color: "#fff"
+  }
+});
+
+const footer_styles = StyleSheet.create({
+  container: {
+    height: "10%",
+    flexDirection: "row",
+    justifyContent: "space-around"
+  },
+  button: {
+    justifyContent: "center",
+    width: "30%",
+    backgroundColor: "skyblue",
+    padding: 10
   }
 });
